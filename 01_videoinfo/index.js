4c2047e819c463f21ac09798f9d0a03c1482f654
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-25 15:05:43 +0900

    videoinfo

--- 01_videoinfo/index.js
+++ 01_videoinfo/index.js
@@ -1,8 +1,35 @@
+const path = require('path');
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    title: 'VideoInfo',
+    width: 800,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.webContents.openDevTools();
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.on('close', () => {
+    mainWindow = null;
+  });
+});
+
+ipcMain.on('video:submit', (event, options) => {
+  ffmpeg.ffprobe(options.path, function (err, metadata) {
+    mainWindow.webContents.send('video:metadata', {
+      duration: metadata.format.duration,
+    });
+  });
 });
