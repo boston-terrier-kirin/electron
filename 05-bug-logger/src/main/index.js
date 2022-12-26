b9ad8e784eaae17208c06f4267970b3a45c79411
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-26 13:55:32 +0900

    05-bug-logger

--- 05-bug-logger/src/main/index.js
+++ 05-bug-logger/src/main/index.js
@@ -1,9 +1,14 @@
 import path from 'path';
-import { BrowserWindow, app, ipcMain } from 'electron';
+import { BrowserWindow, Menu, app, ipcMain } from 'electron';
 
-app.whenReady().then(() => {
-  const mainWindow = new BrowserWindow({
-    width: 1280,
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+let mainWindow;
+
+function createMainWidnow() {
+  mainWindow = new BrowserWindow({
+    width: isDev ? 1400 : 1280,
     height: 800,
     webPreferences: {
       nodeIntegration: true,
@@ -11,11 +16,35 @@ app.whenReady().then(() => {
     },
   });
 
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
   mainWindow.loadFile('dist/index.html');
-  mainWindow.webContents.openDevTools({ mode: 'detach' });
+}
+
+app.whenReady().then(() => {
+  createMainWidnow();
+
+  const mainMenu = Menu.buildFromTemplate([]);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWidnow();
+    }
+  });
 });
 
-app.once('window-all-closed', () => app.quit());
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
 
 ipcMain.on('ping', (event, options) => {
   console.log(options);
